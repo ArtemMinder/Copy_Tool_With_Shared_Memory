098c846b0e653245ea488808850560c30ce4fedb
ArtemMinder <artsiom.minder@gmail.com>
2023-11-24 10:44:19 +0100

    remowe test code

--- main.cpp
+++ main.cpp
@@ -12,56 +12,52 @@
 
 using namespace boost::interprocess;
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    if (argc != 4) 
-    {
-        std::cout << "Usage: " << argv[0] << " <input_file> <output_file> <shared_memory_object_name>" << std::endl;
-      //  return 1;
-    }
+	if (argc != 4)
+	{
+		std::cout << "Usage: " << argv[0] << " <input_file> <output_file> <shared_memory_object_name>" << std::endl;
+		return 1;
+	}
 
-   /* const char* inputFileName = argv[1];
-    const char* outputFileName = argv[2];
-    const char* sharedMemoryObjectName = argv[3];*/
+	const char* inputFileName = argv[1];
+	const char* outputFileName = argv[2];
+	const char* sharedMemoryObjectName = argv[3];
 
-    const char* inputFileName = "source.txt";
-    const char* outputFileName = "target.txt";
-    const char* sharedMemoryObjectName = "shm";
-   
-    FileProcessorFactory* factory;
+	FileProcessorFactory* factory;
 
-    try 
-    {
-        shared_memory_object sharedMemory(open_only, sharedMemoryObjectName, read_write);
-        mapped_region region(sharedMemory, read_only);
-        SharedData* sharedData= static_cast<SharedData*>(region.get_address());
+	try
+	{
+		shared_memory_object sharedMemory(open_only, sharedMemoryObjectName, read_write);
+		mapped_region region(sharedMemory, read_only);
+		SharedData* sharedData = static_cast<SharedData*>(region.get_address());
 
-        bool dataExist = false;
+		bool dataExist = false;
 
-         dataExist = sharedData->dataExist;
+		dataExist = sharedData->dataExist;
 
 
-        if (dataExist) 
-        {
-            std::cout << "Shared memory exists with data. Starting write process.\n";
-            factory = new FileReaderFactory();
-        }
-        else {
-            std::cout << "Shared memory exists without data. Starting read process.\n";
-            factory = new FileWriterFactory();
-        }
-    }
-    catch (const interprocess_exception& ex) 
-    {
-        std::cout << "Shared memory does not exist. Starting write process.\n";
-        factory = new FileWriterFactory();
-    }
+		if (dataExist)
+		{
+			std::cout << "Shared memory exists with data. Starting write process.\n";
+			factory = new FileReaderFactory();
+		}
+		else {
+			std::cout << "Shared memory exists without data. Starting read process.\n";
+			factory = new FileWriterFactory();
+		}
+	}
+	catch (const interprocess_exception& ex)
+	{
+		std::cout << "Shared memory does not exist. Starting write process.\n";
+		factory = new FileWriterFactory();
+	}
 
-    FileProcessor* processor = factory->createFileProcessor();
-    processor->processFile(inputFileName, outputFileName, sharedMemoryObjectName);
+	FileProcessor* processor = factory->createFileProcessor();
+	processor->processFile(inputFileName, outputFileName, sharedMemoryObjectName);
 
-    delete processor;
-    delete factory;
+	delete processor;
+	delete factory;
 
-    return 0;
+	return 0;
 }
